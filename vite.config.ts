14bbf9afb31833b313fc6eecffe80747e9db095e
danielretfordit <danielretford@gmail.com>
2026-04-07 17:27:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/broadcast-lab-kit/', // <--- ДОБАВЬТЕ ЭТУ СТРОКУ
   server: {
     host: "::",
     port: 8080,
